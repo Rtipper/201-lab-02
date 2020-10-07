46fd6892d8f3c1c446999caa794d2d53d80440aa
RYAN <RTIPPER44@GMAIL.COM>
2020-10-06 19:35:55 -0700

    updating lab 3 for no responses

--- js/app.js
+++ js/app.js
@@ -8,14 +8,21 @@ alert('Greetings ' + username)
 var homeLocation = prompt('Am I from outer space?').toLowerCase();
 if (homeLocation === 'yes' || homeLocation === 'y') {
   // console.log('Yes! I live in outer space');
-  alert('Yes! I live in outer space');
-}
+  alert('Yes! I live in outer space.');
+} else if (homeLocation === 'no' || homeLocation === 'n') {
+    // console.log('That is incorrect as I am indeed from outer space');
+    alert('That is incorrect as I am indeed from outer space.');
+  }
+
 
 // Ryan - Question 2:
 var planet = prompt('Am I from the planet Neptune?').toLowerCase();
 if (planet === 'yes' || planet === 'y') {
   // console.log('Yes! I am indeed from Neptune');
   alert('Yes! I am indeed from Neptune');
+} else if (planet === 'no' || planet === 'n') {
+  // console.log('Incorrect, I do indeed hail from the wounderous Neptune!')
+  alert('Incorrect, I do indeed hail from the wonderous Neptune!');
 }
 
 // Ryan - Question 3:
@@ -23,6 +30,9 @@ var rocketShip = prompt('Did I first arrive there via rocket ship?').toLowerCase
 if (rocketShip === 'yes' || rocketShip === 'y') {
   // console.log('Yes! I did arrive by rocket ship');
   alert('Yes! I did first arrive there by rocket ship');
+} else if (rocketShip === 'no' || rocketShip === 'n') {
+  // console.log('That is incorrect. I did indeed arrive on a rocket ship. Space is cold!')
+  alert('That is incorrect. I did indeed arrive on a rocket ship. Space is cold!');
 }
 
 // Ryan - Question 4:
@@ -30,6 +40,9 @@ var returnHome = prompt('Will you help me return home?').toLowerCase();
 if (returnHome === 'yes' || returnHome === 'y') {
   // console.log('Yes? Hooray! I can/t wait to retun home!');
   alert('Yes? Hooray! I cant wait to retun home!');
+} else if (returnHome === 'no' || returnHome === 'n') {
+  // console.log ('Excuse me? I am a guest on your planet and you are being so rude as to not help!?')
+  alert('Excuse me? I am a guest on your planet and you are being so rude as to not help!?');
 }
 
 // Ryan - Question 5:
@@ -37,13 +50,17 @@ var doYouHave = prompt('Wait, do you have an extra rocket ship?').toLowerCase();
 if (doYouHave === 'yes' || doYouHave === 'y') {
   // console.log('This is wonderful news indeed!');
   alert('This is wonderful news indeed!');
+} else if (doYouHave === 'no' || returnHome === 'n') {
+  // console.log ('You dont? Hmm. This is a problem.');
+  alert('You dont? Hmmm. This is a problem.');
 }
+
 
 // Final Message
 // alert('It is wonderful to meet you ' + username + '. ' + 'I am so thrilled you could tell I am from Neptune' + '. ' + 'Did my accent give it away' + '? ' + 'I can tell you realized I am now stranded here no thanks in part to the smoldering wreckage of metal behind me that used to be my ship' + '. ' + 'I am so happy to hear you have a spare rocket ship that can take me home' + '! ' + 'Shall we lift off to the cosmos above' + '? ');
 
 // Final Message with Template Literal
-alert(`It is wonderful to meet you ${username}! I am so thrilled you could tell I am from Neptune. Did my accent give it away? I can tell you realized I am now stranded here not thanks in part to the smoldering wreckage of metal behind me that used to be my ship. I am so happy to hear you have a spare rocket ship that can take me home! Shall we lift off to the cosmos above?`);
+alert(`Either way, it is wonderful to meet you ${username}! I am so thrilled you now know I am from Neptune. Did my accent give it away? Obviously I am now stranded here no thanks in part to the smoldering wreckage of metal behind me that used to be my ship. Weather you do or do not have a spare rocket ship that can take me home, I shall find my way back! Shall we lift off to the cosmos above?`);
 
 // Ryan - Question 6 (Lab 3 Update):
 var numberRange = 10;
@@ -58,21 +75,21 @@ while (usersGuess != targetNumber && numberOfGuesses < maxTries) {
 
   //Check if the user entered a number
   if(isNaN(usersGuess)){
-      alert("Dude... That's not even a number.");
+      alert(`Nice try ${username} but that aint even a number.`);
   } 
   else
   {
       //Check if the user's guess was too high or too low
       if(usersGuess > targetNumber){
-          alert("Too high");
+          alert(`Too high, ${username}`);
       }
       if(usersGuess < targetNumber){
-          alert("Too low");
+          alert(`Too low, ${username}`);
       }
 
       //The user guessed correctly
       if (usersGuess == targetNumber){
-        alert(`Well done, that is correct! The random number was ${targetNumber}! It took you ${counter} attempts to get the correct number`);
+        alert(`Well done ${username}, that is correct! The random number was ${targetNumber}! It took you ${counter} attempts to get the correct number`);
       }
   }
 
